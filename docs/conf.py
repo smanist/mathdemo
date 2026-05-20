e510cdb7ce73e4aeb3081f06c8b71336bbecf037
Daning Huang <huangdn.nuaa@gmail.com>
2026-05-20 15:57:08 -0400

    Fix style in ODE

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,24 @@ html_js_files = [
     "js/course-interactives.js",
 ]
 
+mathjax3_config = {
+    "tex": {
+        "macros": {
+            "vf": r"\mathbf{f}",
+            "vk": r"\mathbf{k}",
+            "vy": r"\mathbf{y}",
+            "vz": r"\mathbf{z}",
+            "dd": r"\mathrm{d}",
+            "ppf": [r"\frac{\partial #1}{\partial #2}", 2],
+            "ddf": [r"\frac{\mathrm{d} #1}{\mathrm{d} #2}", 2],
+            "norm": [r"\left\lVert #1 \right\rVert", 1],
+            "Cr": [r"{\color{red} #1}", 1],
+            "Cg": [r"{\color{green} #1}", 1],
+            "Cb": [r"{\color{blue} #1}", 1],
+        },
+    },
+}
+
 html_theme_options = {
     "description": "Static Sphinx/MyST notes with browser-side examples",
     "fixed_sidebar": True,
